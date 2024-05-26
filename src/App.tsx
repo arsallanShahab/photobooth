be07313de02b6968d62c1339342714a33051e65c
Arsallan Shahab <mdarsallan@gmail.com>
2024-05-26 20:28:39 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import FlexContainer from "./components/FlexContainer";
+
+function App() {
+  return (
+    <FlexContainer variant="row-center" className="h-screen w-full">
+      Main
+    </FlexContainer>
+  );
+}
+export default App;
