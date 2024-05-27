acd59ade614aad336c9f8f0abfc2549fb62f0708
Arsallan Shahab <mdarsallan@gmail.com>
2024-05-27 13:17:22 +0530

    feat: Add NextUIProvider and framer-motion dependencies
    This commit adds the "@nextui-org/react" and "framer-motion" dependencies to the project. These dependencies are required for implementing UI components and animations in the application. The "@nextui-org/react" package is added with version "^2.3.6" and "framer-motion" package is added with version "^11.2.6".

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+import Providers from "./components/Providers.tsx";
 import "./index.css";
 import ErrorPage from "./pages/error-page.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
     errorElement: <ErrorPage />,
+    element: <Providers />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+    ],
   },
 ]);
 
